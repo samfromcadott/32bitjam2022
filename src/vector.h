e757171cda8a2f7c0ce00eb02a2a3392bf54ecfc
samfromcadott <samahstan@gmail.com>
2022-10-17 11:05:57 -0500

    Added vector.h

---
+++ src/vector.h
@@ -0,0 +1,15 @@
+typedef struct {
+	short x, y;
+} vec2;
+
+inline vec2 vec_add(const vec2 a, const vec2 b) {
+	return {a.x + b.x, a.y + b.y};
+}
+
+inline vec2 vec_sub(const vec2 a, const vec2 b) {
+	return {a.x - b.x, a.y - b.y};
+}
+
+inline vec2 vec_scale(const vec2 v, const short s) {
+	return {v.x * s, v.y * s};
+}
